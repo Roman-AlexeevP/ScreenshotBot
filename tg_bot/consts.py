c569b594590348358c7e2e2b5dd7d61579e6a6db
lebelius <alexeevroma@gmail.com>
2022-08-11 00:16:33 +0300

    feature(bot): add consts for api

--- tg_bot/consts.py
+++ tg_bot/consts.py
@@ -1,2 +1,5 @@
 MEDIA_DIR_NAME = "screenshots/"
 VALIDE_RESPONSE_STASUSES = {200, 201, 202, 203, 204, 205, 206, 300, 301, 302, 303, 304, 305, 306, 307}
+WHOIS_API_URL = "http://ip-api.com/json/"
+WHOIS_API_FIELDS_PARAMETER = "fields"
+WHOIS_API_FIELDS = ["status", "message", "continent", "country", "regionName", "city", "isp", "org", "query"]
