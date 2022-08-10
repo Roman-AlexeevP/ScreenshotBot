7ecd4c1d3bb10b6af89f373cad450766b20406d2
lebelius <alexeevroma@gmail.com>
2022-08-11 00:17:00 +0300

    feature(keybord): create keyboard factory for whois button

---
+++ tg_bot/keyboard.py
@@ -0,0 +1,19 @@
+import urllib
+
+from aiogram import types
+from aiogram.utils.callback_data import CallbackData
+
+callback_factory = CallbackData("whois", "action", "url")
+
+
+def whois_keybord_factory(url: str) -> types.InlineKeyboardMarkup:
+    domain = urllib.parse.urlparse(url).netloc
+    whois_button = types.InlineKeyboardButton(
+        text="Подробнее(WHOIS)",
+        callback_data=callback_factory.new(action="get_whois", url=domain)
+    )
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(whois_button)
+    return keyboard
+
+
