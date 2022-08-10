652414d7b5ab1eb41b3c64a344fa933f4f9bd37e
lebelius <alexeevroma@gmail.com>
2022-08-11 00:17:34 +0300

    feature(handlers): add whois handler, move text factory to another file

--- tg_bot/handlers/users.py
+++ tg_bot/handlers/users.py
@@ -1,11 +1,13 @@
 import logging
 
-import aiogram.utils.markdown as fmt
 from aiogram import Dispatcher
 from aiogram import types
 
 import consts
-from services.screenshots import save_screenchot
+from keyboard import whois_keybord_factory, callback_factory
+from services import screenshots
+from services import whois
+from tg_bot import text_factory
 from tg_bot.validators import urls
 
 logger = logging.getLogger(__name__)
@@ -13,34 +15,40 @@ logger = logging.getLogger(__name__)
 
 async def user_start(message: types.Message):
     logger.info(f"User {message.from_user.id} start work with bot")
-    message_text = "Привет! Меня зовут ImagerClone. Я - Бот для создания веб-скриншотов." \
-                   "\n\nЧтобы получить скриншот - отправьте URL адрес сайта. \n\nKНапример, http://wikipedia.org"
-
-    await message.reply(message_text)
+    await message.reply(text_factory.get_user_start_text())
 
 
 async def on_message(message: types.Message):
     logger.info(f"URL: {message.text} from user: {message.from_user.id}")
+
     is_url_correct = await urls.check_url_status(message.text)
     if not is_url_correct:
         logger.warning(f"Wrong URL:{message.text} from user: {message.from_user.id}")
-        return await message.reply("Неправильная ссылка или формат. Попробуйте еще раз, пример ссылки:"
-                                   r"https://github.com", disable_web_page_preview=True)
+        return await message.reply(text_factory.get_wrong_url_text(), disable_web_page_preview=True)
 
     temp_photo = types.InputFile(path_or_bytesio=f"{consts.MEDIA_DIR_NAME}loading.png")
-    temporary_message = await message.reply_photo(photo=temp_photo, caption="Запрос отправлен на сайт")
+    temporary_message = await message.answer_photo(photo=temp_photo, caption="Запрос отправлен на сайт")
 
-    page_detail = await save_screenchot(message=message)
-    text = fmt.text(
-        fmt.text(f"{page_detail.title}"),
-        fmt.text("Веб-сайт", page_detail.url, sep=": "),
-        fmt.text("Время обработки", f"{page_detail.calculation_time_sec:.1f} сек.", sep=": "),
-        sep="\n\n"
-    )
-    photo = types.InputMediaPhoto(media=types.InputFile(page_detail.screenshot_path), caption=text)
-    await temporary_message.edit_media(media=photo)
+    page_detail = await screenshots.save_screenchot(message=message)
+
+    photo = types.InputMediaPhoto(media=types.InputFile(page_detail.screenshot_path),
+                                  caption=text_factory.get_screenshot_caption(page_detail))
+
+    keyboard = whois_keybord_factory(page_detail.url)
+
+    await temporary_message.edit_media(media=photo, reply_markup=keyboard)
+
+
+async def whois_callback(call: types.CallbackQuery, callback_data: dict):
+    url = callback_data["url"]
+    logger.info(f"Get WHOIS URL: {url} from user: {call.from_user.id}")
+    whois_info = await whois.get_whois_response(url)
+    if not whois_info.status:
+        await call.answer(text_factory.get_error_whois_text(whois_info), show_alert=True)
+    await call.answer(text_factory.get_whois_text(whois_info), show_alert=True)
 
 
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"], state="*")
     dp.register_message_handler(on_message, content_types=types.ContentType.TEXT)
+    dp.register_callback_query_handler(whois_callback, callback_factory.filter())
---
+++ tg_bot/text_factory.py
@@ -0,0 +1,54 @@
+from aiogram.utils import markdown as fmt
+
+from services import screenshots, whois
+
+
+def get_user_start_text() -> str:
+    text = fmt.text(
+        fmt.text("Привет! Меня зовут ImagerClone. Я - Бот для создания веб-скриншотов."),
+        fmt.text("Чтобы получить скриншот - отправьте URL адрес сайта."),
+        fmt.text("Например, http://wikipedia.org"),
+        sep="\n\n"
+    )
+    return text
+
+
+def get_screenshot_caption(page_detail: screenshots.PageDetail):
+    text = fmt.text(
+        fmt.text(f"{page_detail.title}"),
+        fmt.text(f"Веб-сайт: {page_detail.url}"),
+        fmt.text(f"Время обработки: {page_detail.calculation_time_sec:.1f} сек."),
+        sep="\n\n"
+    )
+    return text
+
+
+def get_error_whois_text(whois_info: whois.WhoisInfo) -> str:
+    text = fmt.text(
+        fmt.text(f"URL: {whois_info.url}"),
+        fmt.text(f"Статус: {whois_info.status}"),
+        fmt.text(f"Текст ошибки: {whois_info.message}"),
+        sep="\n\n"
+    )
+    return text
+
+
+def get_whois_text(whois_info: whois.WhoisInfo) -> str:
+    text = fmt.text(
+        fmt.text(f"IP: {whois_info.query}"),
+        fmt.text(f"Страна: {whois_info.country}"),
+        fmt.text(f"Континент: {whois_info.continent}"),
+        fmt.text(f"Город: {whois_info.city}"),
+        fmt.text(f"Провайдер: {whois_info.isp}"),
+        fmt.text(f"Организация: {whois_info.org}"),
+        sep="\n\n"
+    )
+    return text
+
+def get_wrong_url_text() -> str:
+    text = fmt.text(
+        fmt.text("Неправильная ссылка или формат. Попробуйте еще раз, пример ссылки:"),
+        fmt.text(r"https://github.com"),
+        sep="\n"
+    )
+    return text
